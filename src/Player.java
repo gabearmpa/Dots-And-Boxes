16042a36d22405d53769b65986afe0635d9ad1be
Stephen S <stestoltz@users.noreply.github.com>
2020-04-02 14:59:27 -0400

    Work on Game, starting to set up UI

---
+++ src/Player.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Player {
+	
+	private int color;
+	
+	public Player(int color) {
+		this.color = color;
+	}
+	
+	public Position nextPlay(Board b, List<Position> legalPlays) {
+		
+	}
+
+}
